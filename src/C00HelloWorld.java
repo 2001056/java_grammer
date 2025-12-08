d9086f870fd071bae879bc9c87e00101900234b8
2001056 <lbchan1027@gmail.com>
2025-12-08 12:25:44 +0900

    Initial commit

---
+++ src/C00HelloWorld.java
@@ -0,0 +1,13 @@
+
+// 클래스의 명명규칙
+// 1) 반드시 public class명과 파일명이 일치해야함
+// 2) 일반적으로 대문자알파벳으로 시작
+
+
+public class C00HelloWorld {
+//    1) main메서드는 java파일 실행시 실행의 시작점이자, 가장 먼저 실행되는 메서드
+//    2) public static void : 어디에서든 접근 가능하고,
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
