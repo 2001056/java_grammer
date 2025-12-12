e7c6dc100884be636d850f49d25d55141efabb64
2001056 <lbchan1027@gmail.com>
2025-12-12 12:10:47 +0900

    a

--- src/C01Basic/C11Map.java
+++ src/C01Basic/C11Map.java
@@ -1,4 +1,11 @@
 package C01Basic;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class C11Map {
+    public static void main(String[] args) {
+//        map : key,value로 이루어진 자료구조
+        Map<String,Integer> 변수명 = new HashMap<>();
+    }
 }
