d693a7c45e1af9876dca88860fd2835683110059
2001056 <lbchan1027@gmail.com>
2025-12-12 12:16:47 +0900

    추가수정

--- src/C01Basic/C11Map.java
+++ src/C01Basic/C11Map.java
@@ -6,6 +6,11 @@ import java.util.Map;
 public class C11Map {
     public static void main(String[] args) {
 //        map : key,value로 이루어진 자료구조
-        Map<String,Integer> 변수명 = new HashMap<>();
+        Map<String,Integer> sports = new HashMap<>();
+        sports.put("농구",2);
+        sports.put("축구",3);
+        sports.put("배구",2);
+        System.out.println(sports);
+
     }
 }
