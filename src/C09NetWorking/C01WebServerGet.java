ab76373adcdad78bd6ddbd8c7ad42b819366c13f
2001056 <lbchan1027@gmail.com>
2025-12-29 09:15:03 +0900

    co

---
+++ src/C09NetWorking/C01WebServerGet.java
@@ -0,0 +1,33 @@
+package C09NetWorking;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+    public class C01WebServerGet {
+        public static void main(String[] args) throws IOException {
+
+//        사용자 : 크롬을 통해 http://서버url get 요청 (http 요청 문서 조립 필요)
+//        서버 : "hello world java" 문자열을 응답 (http 응답 문서 조립 필요)
+
+//        소켓이란 서버와 사용자가 통신을 하기 위한 네트워크 통신의 끝점을 나타내는 개념
+            ServerSocket serverSocket = new ServerSocket(8080);
+            System.out.println("서버 시작");
+
+            while (true){
+//            accept : 사용자의 연결 요청 수락
+//            socket 객체 안에는 사용자가 보내온 http 요청 문서 및 사용자 정보(ip등)
+                Socket socket = serverSocket.accept();
+
+//            사용자에게 서버의 응답 메시지 전송
+                String response = "HTTP/1.1 200 OK\r\n\r\n" + "hello world java";
+                socket.getOutputStream().write(response.getBytes(StandardCharsets.UTF_8));
+//            flush란 일반적으로 변경사항을 확정하는 것을 의미
+                socket.getOutputStream().flush();
+                socket.close();
+            }
+
+        }
+    }
+
